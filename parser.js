211df83c59b7d7c9d88181c23daceaf825383451
minap <minap@DESKTOP-1ENRRSG>
2019-10-19 17:29:25 +0200

    spaß mit parsern teil 1

--- parser.js
+++ parser.js
@@ -52,19 +52,73 @@ function parse(string){
 var saveBtn = document.getElementById("save");
 saveBtn.addEventListener("click", function(){
     var saveTxt = "";
-    player = "";
-    var player_array = document.getElementById("kids").value.split('\n');
+    var selection = document.getElementById("select_1").value;
     var team_rot = []; 
     var team_blau = [];
-    for (var i = 0; i < player_array.length; i++) {
-        
+    var string1 = "";
+    var string2 = "";
+    if (selection == "1") {
+        // Zahlen
+        var team1_size;
+        var team2_size;
+        var int = document.getElementById("kids").value;
+        team1_size = Math.ceil(int /2);
+        team2_size = Math.floor(int /2);
+        string1 ="1";
+        string2 ="1";
+        var loop;
+        for (loop = 2; loop < team2_size; loop++) {
+            string1 += ","+loop;
+            string2 += ","+loop;
+        }
+        if (team1_size > team2_size) {
+            loop++;
+            string1 += ","+loop;
+        }
+        saveTxt += string1+"\n"+string2;
     }
-    document.getElementById("kids").value;
-    saveTxt += player +"\n";
+    else {
+        // Namen
+        var schueler_namen = document.getElementById("schueler_text").split("\n");
+        schueler_namen = shuffle(schueler_namen);
+        var max_size = Math.ceil(schueler_namen.size / 2);
+        team_blau = schueler_namen.slice(0,max_size);
+        if (schueler_namen.size % 2 == 1) {
+            team_rot = schueler_name.slice(0, -(max_size -1));
+        }
+        else team_rot = schueler_name.slice(0, -(max_size));
+        string1 += team_blau[0];
+        string2 += team_rot[1];
+        for (var loopi = 1; loopi < team_blau; loopi++) {
+            string1+= ","+team_blau[loopi];
+        }
+        for (var loopi2 = 1; loopi2 < team_blau; loopi2++) {
+            string2+= ","+team_rot[loopi];
+        }
+        saveTxt += string1+"\n"+string2;
+    }
     saveTxt += aufgaben;
     download("Klasse1", saveTxt);
 }); 
 
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  }
 
 function download(filename, text) {
     var element = document.createElement('a');
